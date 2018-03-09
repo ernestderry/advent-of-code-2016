137420aca8cf5af996930b1ac86b6499be3f9cf4
Ernest Derry <EDERR@travisperkins.com>
2018-03-09 13:08:46 +0000

    break code up into methods

--- src/day4/Room.java
+++ src/day4/Room.java
@@ -10,31 +10,41 @@ import java.util.Map;
 
 public class Room {
 
-	public static boolean isReal(String nameSectorAndChecksum) {
+	public static boolean isReal(String room) {
 
-		if ("".equals(nameSectorAndChecksum)) {
+		if ("".equals(room)) {
 			return false;
 		}
 
-		int checksumStartPos = nameSectorAndChecksum.indexOf('[');
-		int checksumEndPos = nameSectorAndChecksum.indexOf(']');
+		String checksum = extractChecksum(room);
 
-		String checksum = nameSectorAndChecksum.substring(checksumStartPos + 1, checksumEndPos);
+		HashMap<Character, Integer> letterCounts = buildLetterCountsFromName(room);
 
-		String nameParts = nameSectorAndChecksum.substring(0, nameSectorAndChecksum.lastIndexOf('-'));
+		List<Map.Entry<Character, Integer>> letterList = createListOfSortedLetterCounts(letterCounts);
 
-		char[] nameLetters = nameParts.toCharArray();
+		String concatenatedLetters = concatenateFirst5MostPopularLettersTogether(letterList);
 
-		HashMap<Character, Integer> letterCounts = new HashMap<Character, Integer>();
-		for (char letter : nameLetters) {
-			if (letterCounts.containsKey(letter)) {
-				int count = letterCounts.get(letter);
-				letterCounts.put(letter, count + 1);
-			} else {
-				letterCounts.put(letter, 1);
+		return checksum.equals(concatenatedLetters);
+	}
+
+	private static String concatenateFirst5MostPopularLettersTogether(List<Map.Entry<Character, Integer>> letterList) {
+		String concatenatedNames = "";
+
+		for (Iterator<Map.Entry<Character, Integer>> it = letterList.iterator(); it.hasNext();) {
+			Map.Entry<Character, Integer> entry = (Map.Entry<Character, Integer>) it.next();
+			Character letter = (Character) entry.getKey();
+			if (letter != '-') {
+				concatenatedNames += letter;
+			}
+			if (concatenatedNames.length() == 5) {
+				break;
 			}
 		}
+		return concatenatedNames;
+	}
 
+	private static List<Map.Entry<Character, Integer>> createListOfSortedLetterCounts(
+			HashMap<Character, Integer> letterCounts) {
 		List<Map.Entry<Character, Integer>> letterList = new LinkedList<Map.Entry<Character, Integer>>(
 				letterCounts.entrySet());
 
@@ -49,21 +59,31 @@ public class Room {
 				}
 			}
 		});
+		return letterList;
+	}
 
-		String concatenatedNames = "";
+	private static HashMap<Character, Integer> buildLetterCountsFromName(String room) {
+		String nameParts = room.substring(0, room.lastIndexOf('-'));
 
-		for (Iterator<Map.Entry<Character, Integer>> it = letterList.iterator(); it.hasNext();) {
-			Map.Entry<Character, Integer> entry = (Map.Entry<Character, Integer>) it.next();
-			Character letter = (Character) entry.getKey();
-			if (letter != '-') {
-				concatenatedNames += letter;
-			}
-			if (concatenatedNames.length() == 5) {
-				break;
+		char[] nameLetters = nameParts.toCharArray();
+
+		HashMap<Character, Integer> letterCounts = new HashMap<Character, Integer>();
+		for (char letter : nameLetters) {
+			if (letterCounts.containsKey(letter)) {
+				int count = letterCounts.get(letter);
+				letterCounts.put(letter, count + 1);
+			} else {
+				letterCounts.put(letter, 1);
 			}
 		}
+		return letterCounts;
+	}
 
-		return checksum.equals(concatenatedNames);
+	private static String extractChecksum(String room) {
+		int checksumStartPos = room.indexOf('[');
+		int checksumEndPos = room.indexOf(']');
+
+		return room.substring(checksumStartPos + 1, checksumEndPos);		
 	}
 
 	public static int getSector(String nameSectorAndChecksum) {
